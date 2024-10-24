7490eb3a7dffe4a3eb38589bcfe7de931547840e
prof.rapos <eric.rapos@ontariotechu.ca>
2024-10-24 12:29:55 -0400

    add post lecture content for 7b 001

--- Week 7/Lecture_7B_001/src/QuickSort.java
+++ Week 7/Lecture_7B_001/src/QuickSort.java
@@ -4,12 +4,14 @@ public class QuickSort {
 
   public static void quickSort(int[] arr, int left, int right) {
     // make sure there is something still to sort (not a single element)
+    if (left < right) {
+      // partition the array
+      int pValue = partition(arr, left, right);
 
-    // partition the array
-
-
-    //recursively sort two partitions
-
+      //recursively sort two partitions
+      quickSort(arr, left, pValue - 1);
+      quickSort(arr, pValue + 1, right);
+    }
   }
 
 
@@ -20,17 +22,25 @@ public class QuickSort {
     int j = right;    // Start from the end of the array
 
     // while there are still values left to compare
+    while (i <= j) {
+      // Move i to the right until we find an element greater than or equal to the pivot
+      while (i <= j && arr[i] < pivotValue) {
+        i++;
+      }
+      // Move j to the left until we find an element less than or equal to the pivot
+      while (i <= j && arr[j] > pivotValue) {
+        j--;
+      }
+      // If i is still less than or equal to j, we need to swap the out-of-place elements and advance the pointers
+      if (i <= j) {
+        swap(arr,i,j);
+        i++;
+        j--;
+      }
 
-    // Move i to the right until we find an element greater than or equal to the pivot
-
-    // Move j to the left until we find an element less than or equal to the pivot
-
-    // If i is still less than or equal to j, we need to swap the out-of-place elements and advance the pointers
-
-
-
+    }
     // Move the pivot to its final place
-
+    swap(arr,left,j);
     // Return the final position of the pivot
     return j;
   }
@@ -43,6 +53,31 @@ public class QuickSort {
   }
 
   public static void main(String[] args) {
-
+    int[] arr = new int[]{3,6,4,5,7,1,2};
+    System.out.println("Original: " + Arrays.toString(arr));
+    quickSort(arr,0,arr.length-1);
+    System.out.println("Sorted: " + Arrays.toString(arr));
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
