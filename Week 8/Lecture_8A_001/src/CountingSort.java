44facf1169b249a7f9d34992e993375783474dcc
prof.rapos <eric.rapos@ontariotechu.ca>
2024-10-28 12:29:23 -0400

    post lecture content for 8A 001

--- Week 8/Lecture_8A_001/src/CountingSort.java
+++ Week 8/Lecture_8A_001/src/CountingSort.java
@@ -1,21 +1,56 @@
+import java.util.Arrays;
+
 public class CountingSort {
 
   // Stable Counting Sort with max value as parameter
   public static void countingSort(int[] arr, int maxValue) {
 
+    int shift = findShift(arr);
+    maxValue -= shift;
+
+    shiftArray(arr, -shift);
+
     // create a count array and sorted array
+    int[] count = new int[maxValue + 1];
+    int[] sort = new int[arr.length];
 
     // Step 1: Store the count of each element
-
+    for (int i : arr) {
+      count[i]++;
+    }
 
     // Step 2: Calculate the running total (cumulative count)
-
+    for (int i = 1; i < count.length; i++) {
+      count[i] += count[i-1];
+    }
 
     // Step 3: Build the sorted array in reverse for stability
+    for(int i  = arr.length - 1; i >= 0; i --) {
+      int value = arr[i]; //20
+      count[value]--; //20 -> 19
+      int index = count[value]; //19
+      sort[index] = value; //20 --> sort[19]
+    }
 
+    shiftArray(sort, shift);
 
     // Step 4: Copy sorted array back to original array
+    System.arraycopy(sort,0, arr, 0, arr.length);
 
+  }
+
+  public static void shiftArray(int[] arr, int shift) {
+    for (int i = 0; i < arr.length; i++) {
+      arr[i]+= shift;
+    }
+  }
+
+  public static int findShift(int[] arr) {
+    int min = Integer.MAX_VALUE;
+    for (int i : arr) {
+      min = Math.min(min, i);
+    }
+    return min;
   }
 
   // Test method
